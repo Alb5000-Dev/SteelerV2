ff5796f10546aa2bc3553e4bf54ca667cce0e08e
ic3scream <52409113+ic3scream@users.noreply.github.com>
2020-06-20 14:25:52 +0200

    PlayerControler, Grab, Cam shake
    Added player controler (no prefab);
    Added camera shake (need fix)
    Added grabing system (working)

---
+++ Assets/Scripts/CameraShake/ShakeTransform2.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTransform2 : MonoBehaviour
+{
+    [System.Serializable]
+    public class ShakeEvent
+    {
+        float duration;
+        public float timeRemaining;
+
+        ShakeTransformEventData2 data;
+
+        public ShakeTransformEventData2.Target target
+        {
+            get
+            {
+                return data.target;
+            }
+        }
+
+        Vector3 noiseOffset;
+        public Vector3 noise;
+
+        public ShakeEvent(ShakeTransformEventData2 data)
+        {
+            this.data = data;
+
+            duration = data.duration;
+            timeRemaining = duration;
+
+            float rand = 32.0f;
+
+            noiseOffset.x = Random.Range(0.0f, rand);
+            noiseOffset.y = Random.Range(0.0f, rand);
+            //noiseOffset.z = Random.Range(0.0f, rand);
+        }
+
+        public void Update()
+        {
+            float deltaTime = Time.deltaTime;
+
+            //timeRemaining -= deltaTime;
+
+            float noiseOffsetDelta = deltaTime * data.frequency;
+
+            noiseOffset.x += noiseOffsetDelta;
+            noiseOffset.y += noiseOffsetDelta;
+            noiseOffset.z += noiseOffsetDelta;
+            
+           
+
+            noise.x = Mathf.PerlinNoise(noiseOffset.x, 0.0f);
+            noise.y = Mathf.PerlinNoise(noiseOffset.y, 1.0f);
+            noise.z = Mathf.PerlinNoise(noiseOffset.z, 1.0f);
+            
+            
+
+            noise -= Vector3.one * 0.5f;
+
+            noise *= data.amplitude;           
+            
+            float agePercent = 1.0f - (timeRemaining / duration);
+            noise *= data.blendOverLifetime.Evaluate(agePercent);
+        }
+        public bool IsAlive()
+        {
+            return timeRemaining > 0.0f;
+        }
+    }
+
+    // ...
+
+    List<ShakeEvent> shakeEvents = new List<ShakeEvent>();
+
+    // ...
+
+    public void AddShakeEvent(ShakeTransformEventData2 data)
+    {
+        shakeEvents.Add(new ShakeEvent(data));
+    }
+    public void AddShakeEvent(float amplitude, float frequency, float duration, AnimationCurve blendOverLifetime, ShakeTransformEventData2.Target target)
+    {
+        ShakeTransformEventData2 data = ShakeTransformEventData2.CreateInstance<ShakeTransformEventData2>();
+        data.Init(amplitude, frequency, duration, blendOverLifetime, target);
+
+        AddShakeEvent(data);
+    }
+
+    void Update()
+    {
+        Vector3 positionOffset = Vector3.zero;
+        Vector3 rotationOffset = Vector3.zero;
+
+        for (int i = shakeEvents.Count - 1; i != -1; i--)
+        {
+            ShakeEvent se = shakeEvents[i]; se.Update();
+
+            if (se.target == ShakeTransformEventData2.Target.Position)
+            {
+                positionOffset += se.noise;
+            }
+            else
+            {
+                rotationOffset += se.noise;
+            }
+
+            if (!se.IsAlive())
+            {
+                shakeEvents.RemoveAt(i);
+            }
+        }
+
+        transform.localPosition = positionOffset;
+        transform.localEulerAngles = rotationOffset;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    #region Singleton
+    public static PlayerMove instance;
+    #endregion
+    [SerializeField] private string horizontalInputName, verticalInputName;
+    public float movementSpeed;
+
+    private Rigidbody charRB;
+
+    public float walkingSpeed;
+    private float oldWalkingSpeed;
+    //public float runningSpeed;
+    [SerializeField] private float movementBuildUp;
+    //public bool grounded;
+    Vector3 moveDir;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        oldWalkingSpeed = walkingSpeed;
+        charRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {     
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        
+        moveDir = new Vector3(horizInput , charRB.velocity.y, vertInput);
+        moveDir = transform.TransformDirection(moveDir);
+        
+        charRB.velocity = Vector3.Lerp(charRB.velocity, moveDir, Time.fixedDeltaTime * 20f); ;
+        SetMovementSpeed();
+    }
+
+    private void SetMovementSpeed()
+    {
+        /*if (Input.GetKey(KeyCode.LeftShift) && movementSpeed!= 0) {
+            movementSpeed = Mathf.Lerp(movementSpeed, runningSpeed, movementBuildUp);
+        }*/
+        
+        if (Input.GetAxisRaw(horizontalInputName) == 0 && Input.GetAxisRaw(verticalInputName) == 0) {
+            movementSpeed = 0f;
+        }
+        else
+        {
+            movementSpeed = Mathf.Lerp(movementSpeed, walkingSpeed, movementBuildUp); ;
+        }
+    }
+
+    public void ChangeSpeed(float value) {
+        walkingSpeed = value;
+    }
+
+    public void ResetSpeed() {
+        walkingSpeed = oldWalkingSpeed;
+    }
+}
