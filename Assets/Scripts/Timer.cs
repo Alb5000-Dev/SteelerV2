b860115995f1ac348269e5052c3f0e633ed0f43d
Alb5000-Dev <52410784+Alb5000-Dev@users.noreply.github.com>
2020-06-22 14:40:30 +0200

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    public float thiefTime;
+    public float ownerTime;
+
+    public float timeLeft;
+
+    public Text timer;
+
+    IEnumerator timerCoroutine(float timeToGo)
+    {
+        timeLeft = timeToGo;
+        while (timeLeft >= 0)
+        {
+            yield return new WaitForEndOfFrame();
+            timeLeft -= Time.unscaledDeltaTime;
+            timer.text = Mathf.Round(timeLeft).ToString();
+        }
+        timer.text = "0";
+        if (timeToGo == ownerTime)
+        {
+            //Changement de personnage
+            StartCoroutine(timerCoroutine(thiefTime));
+        }
+        else
+        {
+            //Fin de Game
+            timer.text = "";
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(timerCoroutine(ownerTime));
+        }
+    }
+}
